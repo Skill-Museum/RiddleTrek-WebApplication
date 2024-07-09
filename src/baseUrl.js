b02ef2263377ec562bea52c896b1ef23dd60a1c3
Pratik Karanjit <107355273+Pratik-Karanjit@users.noreply.github.com>
2024-07-09 19:17:19 +0545

    Update baseUrl.js

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,4 +1,2 @@
 // export const BASE_URL = "http://localhost:8000/upload/";
-// export const BASE_URL = "https://riddle-backend-e1sm.onrender.com/upload/";
-
-export const BASE_URL = "https://b31e-103-41-172-195.ngrok-free.app/upload/";
+export const BASE_URL = "https://riddle-backend-e1sm.onrender.com/upload/";
