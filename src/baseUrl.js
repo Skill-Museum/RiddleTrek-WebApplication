2633ebc4d4bc488d1c9691e534969ac98472b83b
MOHAMMAD Taushif Reza <112973122+TaushifReza@users.noreply.github.com>
2024-07-10 11:19:08 +0545

    Update baseUrl.js

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "https://riddle-backend-e1sm.onrender.com/upload/";
 
-export const BASE_URL = "https://riddle-backend-yzkk.onrender.com/upload/";
+export const BASE_URL = "https://5f67-103-41-172-192.ngrok-free.app/upload/";
