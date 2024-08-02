77903ee71cddfa91eddc1213ebd11dc9e1befc88
Pratik Karanjit <pratikkaranjit@gmail.com>
2024-08-02 11:19:34 +0545

    add ngrok

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "https://riddle-backend-e1sm.onrender.com/upload/";
 
-export const BASE_URL = "https://5f67-103-41-172-192.ngrok-free.app/upload/";
+export const BASE_URL = "https://d2ae-103-41-172-195.ngrok-free.app/upload/";
